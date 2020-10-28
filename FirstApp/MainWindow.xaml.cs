7e6e493a2cbf01b14d7eb33d7db9a9af7d21c93a
Warchiefen <70097003+Warchiefen@users.noreply.github.com>
2020-10-28 17:10:12 +0100

    Instanciated buttons, textblocks, textbox and listboxes.

--- FirstApp/MainWindow.xaml.cs
+++ FirstApp/MainWindow.xaml.cs
@@ -17,7 +17,20 @@ namespace FirstApp
 {
     public partial class MainWindow : Window
     {
+        private ListBox productListBox;
+        private ListBox ChartList;
         private TextBox discountBox;
+        private TextBlock chart;
+        private TextBlock productDescritpion;
+        private TextBlock productList;
+        private Button addDiscount;
+        private Button order;
+        private Button empty;
+        private Button save;
+        private Button remove;
+        private Button addItem;
+        private Button info;
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -41,20 +54,22 @@ namespace FirstApp
             root.Content = grid;
             grid.Margin = new Thickness(5);
 
+            //I main griden har vi skapat 5 rader.
             grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
             grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
             grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
             grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
             grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
 
-            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+            //I main griden har vi skapat 3 kolumner
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto }); 
             grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(300) });
             grid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
 
             grid.Visibility = Visibility.Visible;
 
             // FÖRSTA KOLUMNEN I GRIDDEN*****************************************************************
-            TextBlock productList = new TextBlock
+            productList = new TextBlock //Detta är titeln som står högst upp "Produktlista"
             {
                 FontSize = 20,
                 Margin = new Thickness(2),
@@ -67,7 +82,7 @@ namespace FirstApp
             Grid.SetRow(productList, 0);
 
             //KOLUMN 2 PRODUKTBESKRIVNING************************************************************************
-            TextBlock productDescritpion = new TextBlock
+            productDescritpion = new TextBlock //Detta är titeln som står högst upp "Produktbeskrivning"
             {
                 FontSize = 20,
                 Margin = new Thickness(2),
@@ -80,7 +95,7 @@ namespace FirstApp
             Grid.SetRow(productDescritpion, 0);
 
             //VARUKORGEN Texten högst upp ************************************************************************
-            TextBlock chart = new TextBlock
+            chart = new TextBlock //Detta är titeln som står högst upp "Varukorgen"
             {
                 FontSize = 20,
                 Margin = new Thickness(2),
@@ -92,7 +107,7 @@ namespace FirstApp
             Grid.SetColumn(chart, 2);
             Grid.SetRow(chart, 0);
 
-            ListBox productListBox = new ListBox
+            productListBox = new ListBox //I den här ListBoxen ska vi visa användaren en lista på valbara produkter från en csv fil.
             {
                 Margin = new Thickness(0, 2, 0, 0),
                 HorizontalAlignment = HorizontalAlignment.Left,
@@ -104,12 +119,12 @@ namespace FirstApp
             Grid.SetRow(productListBox, 1);
 
             //STACK PANEL FÖR KNAPPARNA I PRODUKTLISTAN*****************************************************************************
-            StackPanel addToChart = new StackPanel { Orientation = Orientation.Horizontal };
+            StackPanel addToChart = new StackPanel { Orientation = Orientation.Horizontal }; //Denna StackPanel är skapad för info och lägg till knapparna.
             grid.Children.Add(addToChart);
             Grid.SetRow(addToChart, 2);
             Grid.SetColumn(addToChart, 0);
 
-            Button info = new Button
+            info = new Button //När användaren klickar här så ska produktbeskrivning visas i kolumn 1 (mitten kolumnen)
             {
                 Content = "Visa info",
                 Width = 100,
@@ -117,7 +132,7 @@ namespace FirstApp
             };
             addToChart.Children.Add(info);
 
-            Button addItem = new Button
+            addItem = new Button //Denna knapp ska lägga till den markerade artikeln i varukorgen
             {
                 Content = "Lägg till",
                 Width = 100,
@@ -127,7 +142,7 @@ namespace FirstApp
 
             //LISTA FÖR VARUKORGEN***************************************************************************************************
 
-            ListBox ChartList = new ListBox
+            ChartList = new ListBox //Listbox som ska visa upp de tillagda artiklarna i varukorgen.
             {
                 Margin = new Thickness(0, 2, 0, 0),
                 HorizontalAlignment = HorizontalAlignment.Left,
@@ -138,12 +153,12 @@ namespace FirstApp
             Grid.SetColumn(ChartList, 2);
             Grid.SetRow(ChartList, 1);
 
-            StackPanel discount = new StackPanel { Orientation = Orientation.Horizontal };
+            StackPanel discount = new StackPanel { Orientation = Orientation.Horizontal }; //Skapade en ny stackPanel för rabattfäleten
             grid.Children.Add(discount);
             Grid.SetRow(discount, 2);
             Grid.SetColumn(discount, 2);
 
-            Button addDiscount = new Button
+            addDiscount = new Button //Här är knappen för att lägga till rabattkoden till varukorgen
             {
                 Content = "Lägg till",
                 Width = 50,
@@ -151,63 +166,64 @@ namespace FirstApp
             };
             discount.Children.Add(addDiscount);
 
-            TextBox discountBox = new TextBox
+            discountBox = new TextBox //Inuti denna boxen matar användaren in rabattkoden,      Denna är initierad högs upp
             {
-                Foreground = Brushes.Black,
-                Background = Brushes.LightBlue,
+                Foreground = Brushes.Gray,
+                Background = Brushes.LightGoldenrodYellow,
                 Text = "Rabattkod",
                 Width = 148,
                 HorizontalAlignment = HorizontalAlignment.Center,
                 Margin = new Thickness(0, 2, 0, 0)
             };
             discount.Children.Add(discountBox);
-            discountBox.GotFocus += HasBeenClicked;
+            discountBox.GotFocus += HasBeenClicked; //Skapar en metod för vad som ska hända när användaren klickar i rabatt rutan.
 
 
             //KNAPPAR INUTI STACKPANELEN FÖR VARUKORGEN*******************************************************************************
-            StackPanel buttonChart = new StackPanel { Orientation = Orientation.Horizontal };
+            StackPanel buttonChart = new StackPanel { Orientation = Orientation.Horizontal }; // skapad för att lägga till knapparna under varukorgen
             grid.Children.Add(buttonChart);
             Grid.SetRow(buttonChart, 3);
             Grid.SetColumn(buttonChart, 2);
-            Button one = new Button
+
+            remove = new Button //Ta bort en produkt (genom att markera den och klicka på denna knappen)
             {
                 Content = "Ta bort",
                 Width = 100,
                 Margin = new Thickness(0, 2, 0, 0)
             };
-            buttonChart.Children.Add(one);
+            buttonChart.Children.Add(remove);
 
-            Button two = new Button
+            save = new Button //Spara varukorgen till en csv fil i temp mappen.
             {
                 Content = "Spara",
                 Width = 100,
                 Margin = new Thickness(0, 2, 0, 0)
             };
-            buttonChart.Children.Add(two);
+            buttonChart.Children.Add(save);
 
-            StackPanel secondButtonChart = new StackPanel { Orientation = Orientation.Horizontal };
+            StackPanel secondButtonChart = new StackPanel { Orientation = Orientation.Horizontal }; // skapad för att lägga till knapparna under varukorgen
             grid.Children.Add(secondButtonChart);
             Grid.SetRow(secondButtonChart, 4);
             Grid.SetColumn(secondButtonChart, 2);
 
-            Button three = new Button
+            empty = new Button //TÖM Knappen
             {
                 Content = "Töm",
                 Width = 100,
                 Margin = new Thickness(0, 2, 0, 0)
             };
-            secondButtonChart.Children.Add(three);
+            secondButtonChart.Children.Add(empty);
 
-            Button four = new Button
+            order = new Button //Beställ knappen som ska leda till att användaren får en "Tack förbeställningen"-ruta med detaljer för beställningen
             {
                 Content = "Beställ",
                 Width = 100,
                 Margin = new Thickness(0, 2, 0, 0)
             };
-            secondButtonChart.Children.Add(four);
+            secondButtonChart.Children.Add(order);
             //***********************************************************************************************
         }
-        private void HasBeenClicked(object sender, RoutedEventArgs e)
+        private void HasBeenClicked(object sender, RoutedEventArgs e) //Denna metod gör så att boxen blir tömd när man klickar på den.
         {
             bool hasBeenClicked = false;
             
