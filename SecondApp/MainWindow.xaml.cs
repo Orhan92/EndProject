f7b30526aca6b61b3e32c9cac9dc7f7ff78bd9ac
Warchiefen <70097003+Warchiefen@users.noreply.github.com>
2020-11-15 13:49:31 +0100

    Adjusted comments and structured code. SecondApp Done.

--- SecondApp/MainWindow.xaml.cs
+++ SecondApp/MainWindow.xaml.cs
@@ -115,6 +115,7 @@ namespace SecondApp
             discountGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(200) });
             discountGrid.Visibility = Visibility.Collapsed;
 
+            //LISTOR *******************************************************************
 
             discountList = new List<Discount>();
             productList = new List<Product>();
@@ -122,7 +123,8 @@ namespace SecondApp
             //pictureNames innehåller relativ sökväg till varje bild (Pictures\*.jpg).
             pictureNames = imageNameList();
 
-            //Start Window
+            //Start Window**************************************************************
+
             label = new TextBlock
             {
                 FontSize = 20,
@@ -135,7 +137,7 @@ namespace SecondApp
             Grid.SetColumn(label, 1);
             Grid.SetRow(label, 0);
 
-            //Button stackpanel
+            //Button stackpanel för användaren att välja vad hen vill göra.
             userChoice = new StackPanel { Orientation = Orientation.Vertical };
             startGrid.Children.Add(userChoice);
             Grid.SetColumn(userChoice, 1);
@@ -176,13 +178,16 @@ namespace SecondApp
 
 
             //RABATTÄNDRINGAR
-            //KOLUMN 0 står tom.
+
+            //KOLUMN 0 innehåller inte något.
+
             //KOLUMN 1 RABATT
             newDiscountPanel = new StackPanel { Orientation = Orientation.Vertical };
             discountGrid.Children.Add(newDiscountPanel);
             Grid.SetColumn(newDiscountPanel, 1);
             Grid.SetRow(newDiscountPanel, 1);
 
+            //Lägger in en schysst bild endast för det grafiskt visuella.
             image = AddImage(@"\Pictures\happysmiley.jpg");
             image.Width = 100;
             image.Margin = new Thickness(0, 50, 0, 0);
@@ -318,13 +323,13 @@ namespace SecondApp
             Grid.SetColumn(imageWrapPanel, 0);
             Grid.SetRow(imageWrapPanel, 1);
 
-            //Läser in bilderna ur pictureNames.csv och visar de i WrapPanelen.
+            //Läser in bilderna ur pictureNames listan och visar de i WrapPanelen.
             foreach (string line in pictureNames)
             {
                 Image imageName = AddImage(line);
                 imageName.Width = 35;
-                //imageWrapPanel.Children.Add(imageName);
 
+                //CheckBox som låter användaren klicka på den bild som ska användas.
                 checkBox = new RadioButton
                 {
                     Content = imageName,
@@ -545,7 +550,7 @@ namespace SecondApp
                 }
             }
 
-            //Programmet kollar om titelfältet är tomt eller inte.
+            //Kollar om titelfältet är tomt eller inte.
             if (newTitle.Text == string.Empty)
             {
                 MessageBox.Show("Titelfältet får inte lämnas tomt.");
@@ -553,7 +558,7 @@ namespace SecondApp
             }
             newTitle.Text = newTitle.Text;
 
-            //Programmet kollar nu om beskrivningsfältet är tomt eller inte
+            //Kollar nu om beskrivningsfältet är tomt eller inte
             if (newDescription.Text == string.Empty)
             {
                 MessageBox.Show("Beskrivning får inte lämnas tomt.");
@@ -561,13 +566,13 @@ namespace SecondApp
             }
             newDescription.Text = newDescription.Text;
 
-            //Skapar variabler för att lägga in i klass-konstruktorn efter if-satserna.
+            //Skapar variabler för att lägga in värdena i klass-konstruktorn efter if-satserna.
             string title = newTitle.Text;
             string description = newDescription.Text;
             decimal parsedValue;
             string imageName = newImage.Text;
 
-            //Programmet kollar om priset är rätt inmatat / om värdet är mindre än eller = 0 / om boxen står tom.
+            //Kollar om priset är en siffra || om värdet är mindre än eller = 0 || om boxen står tom.
             if (!decimal.TryParse(newPrice.Text, out parsedValue) || parsedValue <= 0 || newPrice.Text == string.Empty)
             {
                 MessageBox.Show("Priset måste matas in i form av siffror / Priset får inte vara mindre eller lika med 0 / Boxen får inte lämnas tom.");
@@ -575,6 +580,7 @@ namespace SecondApp
             }
             newPrice.Text = newPrice.Text;
 
+            //Kollar om bildfältet står tomt.
             if (newImage.Text == string.Empty)
             {
                 MessageBox.Show("Bildfältet får inte lämnas tom. Vänligen kryssa i en bild som du vill använda dig utav.");
@@ -582,18 +588,22 @@ namespace SecondApp
             }
             newImage.Text = newImage.Text;
 
-            //Rensar föregående lista när man lägger till så att det inte blir dubbelt av samma produkt när användaren lägger till en ny produkt.
+            //Rensar föregående lista när man lägger till för att undvika dubletter. 
             productListBox.Items.Clear();
 
+            //Lägger in variabler som skapades ovan i konstruktorn.
             Product addedProduct = new Product(title, description, parsedValue, imageName);
+
+            //Lägger till objektet i productList
             productList.Add(addedProduct);
 
+            //Skriver ut objektets namn och pris i ListBoxen för användaren.
             foreach (Product p in productList)
             {
                 productListBox.Items.Add(p.Title + " | " + p.Price.ToString("C"));
             }
 
-            //Tömmer boxarna efter att användaren tryckt på lägg till.
+            //Tömmer boxarna efter att användaren tryckt på lägg till för att enkelt lägga till ny p.
             newTitle.Text = string.Empty;
             newDescription.Text = string.Empty;
             newPrice.Text = string.Empty;
@@ -610,7 +620,7 @@ namespace SecondApp
                     return;
                 }
             }
-            //Kollar om användaren inte har knappat in en kod eller om längden på koden är mindre än 3 eller större än 20.
+            //Kollar om användaren har knappat in en kod || om längden på koden är mindre än 3 eller större än 20.
             if (addDiscountCode.Text == string.Empty || addDiscountCode.Text.Length < 3 || addDiscountCode.Text.Length > 20)
             {
                 MessageBox.Show("Din rabattkod uppfyller inte kriterierna (3-20 tecken).");
@@ -619,9 +629,11 @@ namespace SecondApp
             //Om användarens rabattkod är giltigt inmatad så gör vi om de till stora bokstäver.
             addDiscountCode.Text = addDiscountCode.Text.ToUpper();
 
+            //Variabler som ska läggas till i klassens konstruktor.
             decimal parsedValue;
             string discountCode = addDiscountCode.Text;
-            //Kollar om användaren knappar in siffror. Om inte så visa en MessageBox. Likaså om värdet är <=0 eller >=100
+
+            //Kollar om användaren knappar in siffror || om värdet är <= 0 eller >= 100
             if (!decimal.TryParse(addDiscountPercentage.Text, out parsedValue) || parsedValue >= 100 || parsedValue <= 0)
             {
                 MessageBox.Show("Procentmängden behöver matas in i form av siffror och får inte vara under 0 eller över 100.");
@@ -630,13 +642,13 @@ namespace SecondApp
             //Om användaren uppfyller villkoret så görs denna beräkning.
             parsedValue /= 100;
 
-            //Rensar föregående lista när man lägger till så att det inte blir dubbelt av samma lista när användaren lägger till en ny kod.
+            //Rensar föregående lista för att undvika att dubletter visas upp för användaren.
             discountListBox.Items.Clear();
 
-            //Lägger in värdena som användaren matat in i TextBoxes och gör om till ett objekt.
+            //Lägger in variabelvärdena i klassens konstruktor.
             Discount addedDiscount = new Discount(discountCode, parsedValue);
 
-            //Lägger till objektet i discountList.
+            //Lägger till objektet av klassen i discountList.
             discountList.Add(addedDiscount);
 
             //Skriver ut alla objekt från discountList till discountListBox.
@@ -649,6 +661,7 @@ namespace SecondApp
         //Save
         private void ClickedSaveDiscount(object sender, RoutedEventArgs e)
         {
+            //Sparar rabattlistan till csv fil i Temp mappen.
             var csv = new StringBuilder();
             foreach (Discount discount in discountList)
             {
@@ -669,6 +682,7 @@ namespace SecondApp
         }
         private void ClickedSaveProduct(object sender, RoutedEventArgs e)
         {
+            //Sparar produktlistan till csv fil i Temp mappen.
             var csv = new StringBuilder();
             foreach (Product product in productList)
             {
@@ -693,30 +707,37 @@ namespace SecondApp
         //Remove
         private void ClickedRemoveDiscount(object sender, RoutedEventArgs e)
         {
+            //Tar bort den markerade raden ur ListBoxen som visar 'Aktiva Rabattkoder'
             int selectedIndex = discountListBox.SelectedIndex;
             discountListBox.Items.RemoveAt(selectedIndex);
 
+            //Tar bort den markerade raden ur listan.
             discountList.RemoveAt(selectedIndex);
         }
         private void ClickedRemoveProduct(object sender, RoutedEventArgs e)
         {
+            //Tar bort den markerade raden ur ListBoxen som visar 'Produktlista'
             int selectedIndex = productListBox.SelectedIndex;
             productListBox.Items.RemoveAt(selectedIndex);
 
+            //Tar bort den markerade raden ur listan.
             productList.RemoveAt(selectedIndex);
         }
 
         //Startscreen options Discount / And back from Startscreen
         private void ClickedBackFromDiscount(object sender, RoutedEventArgs e)
         {
+            //Vi visar 'Startsektionen' och gömmer 'Rabattsektionen'
             startGrid.Visibility = Visibility.Visible;
             discountGrid.Visibility = Visibility.Collapsed;
         }
         private void ClickedDiscountEditing(object sender, RoutedEventArgs e)
         {
+            //Gömmer 'Startsektionen' och visar 'Rabattsektionen'
             startGrid.Visibility = Visibility.Collapsed;
             discountGrid.Visibility = Visibility.Visible;
 
+            //Läser in rabattlistan när användaren klickar på 'Rabattändringar'
             ReadDiscountList();
 
         }
@@ -724,60 +745,68 @@ namespace SecondApp
         //Startscreen options Products / And back from Startscreen
         private void ClickedBackFromProduct(object sender, RoutedEventArgs e)
         {
+            //Visar 'Startsektionen' och gömmer 'Produktsektionen'
             startGrid.Visibility = Visibility.Visible;
             productGrid.Visibility = Visibility.Collapsed;
         }
         private void ClickedProductEditing(object sender, RoutedEventArgs e)
         {
+            //Gömmer 'Startsektionen' och visar 'Produktsektionen'
             startGrid.Visibility = Visibility.Collapsed;
             productGrid.Visibility = Visibility.Visible;
 
+            //Läser in produktlistan när användaren klickar på 'Produktändringar'
             ReadProductList();
         }
 
         //Exit
         private void ClickedExit(object sender, RoutedEventArgs e)
         {
+            //Avslutar programmet och stänger ner fönstret.
             Application.Current.Shutdown();
         }
 
-
-        //Calling for Methods when user clicks on ClickedProductEditing or ClickedDiscountEditing.
+        //Kallar på dessa metoder när användaren klickar på 'Produktändringar' eller 'Rabattändringar'.
         private void ReadProductList()
         {
+            //Rensar listan och ListBoxen för att unvika dubletter när användaren klickar på 'Produktändringar'
             productList.Clear();
             productListBox.Items.Clear();
+
             try
             {
-                //Läser in produktlistan ur csv från huvudprogrammet (FirstApp).
+                //Läser in sparad produktlista ur csv Temp mappen om filen finns där.
                 productArray = File.ReadAllLines(@"C:\Windows\Temp\savedEditedProducts.csv");
                 ReadProductListFromCSV();
             }
             catch
             {
+                //Om filen INTE finns så läser vi in från huvudprogrammets csv-fil.
                 productArray = File.ReadAllLines("produktLista.csv");
                 ReadProductListFromCSV();
             }
         }
         private void ReadDiscountList()
         {
+            //Rensar listan och ListBoxen för att unvika dubletter när användaren klickar på 'Rabattändringar'
             discountList.Clear();
             discountListBox.Items.Clear();
 
             try
             {
-                //Läser in produktlistan ur csv från huvudprogrammet (FirstApp).
+                //Läser in sparad produktlista ur csv Temp mappen om filen finns där.
                 discountArray = File.ReadAllLines(@"C:\Windows\Temp\savedDiscountList.csv");
                 ReadDiscountListFromCSV();
             }
             catch
             {
+                //Om filen INTE finns så läser vi in från huvudprogrammets csv-fil.
                 discountArray = File.ReadAllLines("rabattKoder.csv");
                 ReadDiscountListFromCSV();
             }
         }
 
-        //Methods for reading product/discount lists.
+        //METODER för att läsa in produktlista och rabattlista
         private void ReadProductListFromCSV()
         {
             //Separerar alla ',' och lägger in de i diverse titel.
@@ -791,9 +820,12 @@ namespace SecondApp
 
                 //För varje rad i csv filen skapar vi ett nytt objekt av klassen.
                 Product products = new Product(productName, productDescription, productPrice, productImage);
+
                 //Lägger till objektet i en lista (titelnamn, beskrivning, bild och pris)
                 productList.Add(products);
             }
+
+            //Skriver ut produktnamn och pris i ListBox så att användaren kan se produkterna.
             foreach (Product p in productList)
             {
                 productListBox.Items.Add(p.Title + " | " + p.Price.ToString("C"));
@@ -810,19 +842,22 @@ namespace SecondApp
 
                 //För varje rad i csv filen skapar vi ett nytt objekt av klassen.
                 Discount discount = new Discount(discountCode, discountPercentage);
-                //Lägger till objektet i en lista (titelnamn, beskrivning, bild och pris)
+
+                //Lägger till objektet i en lista (kod, procentsats)
                 discountList.Add(discount);
             }
+
+            //Skriver ut kod och procentsats i ListBox så att användaren kan se rabattkoderna.
             foreach (Discount d in discountList)
             {
                 discountListBox.Items.Add(d.Code + " | " + (d.DiscountPercentage * 100) + "%");
             }
         }
 
-
+        //METODER för bilder.
         private List<string> imageNameList()
-        //Read picture names from csv and adding into a temporary list.
         {
+            //Read picture names from csv and adding into a temporary list.
             List<string> temp = new List<string>();
             string[] imageNames = File.ReadAllLines("pictureNames.csv");
 
@@ -834,9 +869,9 @@ namespace SecondApp
         }
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
         {
-            //We check if the RadioButton is checked. 
-            //We use a Tag so that the checkbox knows which picture belongs to the checkbox.
-            //And prints it out under "Bildnamn".
+            //Vi kollar om checkboxen är ikryssad.
+            //Använder Tag så att checkboxen vet vilken bild/namn som tillhär till just den checkboxen
+            //Skriver sedan ut bildens namn under "Bildnamn".
             RadioButton checkBox = (RadioButton)sender;
             string imageName = (string)checkBox.Tag;
             newImage.Text = imageName;
